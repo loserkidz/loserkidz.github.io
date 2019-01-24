3c42bfdd67ded0dc6ef3b450ab5b24501f4832d5
loserkidz <46760302+loserkidz@users.noreply.github.com>
2019-01-24 23:44:33 +0700

    Update safelink_home.js

--- safelink/js/safelink_home.js
+++ safelink/js/safelink_home.js
@@ -24,7 +24,7 @@ var xxx=null;
 if(x=="1"){xxx="in-homeowners-or-renters-policy.html"}
 if(x=="2"){xxx="five-predictions-on-insurance-in-newest.html"}
 if(x=="3"){xxx="five-predictions-on-insurance-in-newest_22.html"}
-if(x=="4"){xxx="five-predictions-on-insurance-in-newest_79.html"}a_to_ve[i].href="http://www.redirox.blogspot.com/2019/01/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="4"){xxx="five-predictions-on-insurance-in-newest_79.html"}a_to_ve[i].href="https://www.redirox.blogspot.com/2019/01/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
