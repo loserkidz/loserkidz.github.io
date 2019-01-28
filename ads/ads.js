7d4b58d8749f8f10f13622397c223791d9d48b72
loserkidz <46760302+loserkidz@users.noreply.github.com>
2019-01-28 07:08:06 +0700

    Update ads.js

--- ads/ads.js
+++ ads/ads.js
@@ -1,4 +1,4 @@
-var e = document.createElement('div');
-e.id = 'test-block';
-e.style.display = 'none';
+var e=document.createElement('div');
+e.id='goblog';
+e.style.display='none';
 document.body.appendChild(e);
